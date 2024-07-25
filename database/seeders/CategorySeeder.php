736916f1ed5a68138b844979325a5bb87487e0aa
Anuar Rodriguez Medina <anuarrodriguezmedina@MacBook-Pro-de-Anuar.local>
2024-07-25 00:44:48 -0500

    se amplia el seeder de categorias para tener mas datos

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,16 +14,53 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $category = new Category();
-        $category->name = "Aguas y gaseosas";
+        $category->name = "Bebidas";
+        $category->description = "Todo tipo de bebidas: jugos, gaseosas, agua, etc.";
         $category->save();
 
-        $category2 = new Category();
-        $category2->name = "Carnes";
-        $category2->description = "Productos de carnes";
-        $category2->save();
-        
-        $category3 = new Category();
-        $category3->name = "Limpieza";
-        $category3->save();
+        $category = new Category();
+        $category->name = "Lácteos";
+        $category->description = "Productos lácteos: leche, queso, yogurt, etc.";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Carnes";
+        $category->description = "Carnes frescas y congeladas";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Frutas y Verduras";
+        $category->description = "Frutas y verduras frescas";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Panadería";
+        $category->description = "Productos de panadería: panes, galletas, pasteles, etc.";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Abarrotes";
+        $category->description = "Productos de despensa: arroz, frijoles, pastas, etc.";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Congelados";
+        $category->description = "Productos congelados: helados, vegetales congelados, etc.";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Limpieza";
+        $category->description = "Productos de limpieza para el hogar";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Higiene Personal";
+        $category->description = "Productos de higiene personal: jabón, shampoo, pasta dental, etc.";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Bebidas Alcohólicas";
+        $category->description = "Vinos, cervezas, y otras bebidas alcohólicas";
+        $category->save();
     }
 }
